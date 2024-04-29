fc985e9b8010cb201c883f1fb41be935f9a752bc
tanqueta88 <oliverprim783@gmail.com>
2024-04-29 05:06:35 -0300

    Program Regiones is added.

---
+++ POO/PC3POO/Regiones/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+
+Console.WriteLine("Hello, World!");
+//Implemente una calse region con su nombre y superficie, una clase pais que herede de region
+//Cada pais posee una coleccion de ciudades
+
+var Argentina = new Pais();
+Argentina.OrganizacionPolitica=Pais.Organizacion.Imperio;
+Argentina.Ciudades=new List<Ciudad>();
+
+Argentina.Ciudades.Add(new Ciudad(){Nombre="CABA",Superficie=1234, CiudadCapital=true});
+
+var AmericaDelSur= new Continente();
+AmericaDelSur.Paises= new List<Pais>();
+AmericaDelSur.Pais.add(Argentina);
+AmericaDelSur.Pais.add(new Pais(){Nombre="Urugay",Superficie="456",OrganizacionPolitica=Pais.Organizacion.Republica});
+public class Region{
+    public string Nombre { get; set; }
+    public int Superficie { get; set; }
+}
+
+public class Pais : Region
+{
+    public string Idioma { get; set; }
+    public enum Organizacion
+    {
+        Republica,
+        Monarquia,
+        Imperio,
+        Otro
+    }
+    public Organizacion OrganizacionPolitica { get; set; }
+
+}
+
+public class Ciudad : Region {
+    public bool CiudadCapital { get; set; }
+}
+
+public class Continente : Region {
+    public List<Pais> CiudadCapital { get; set; }
+}
---
+++ POO/PC4POO/CuentasEncapsulado/Program.cs
@@ -0,0 +1,61 @@
+﻿// var cuenta1=new Cuenta();
+var cuenta2=new CtaCtr();
+var cuenta3=new CajaAhorro();
+cuenta2.Retirar();
+cuenta3.Retirar();
+
+public abstract class Cuenta
+{
+    public int DNI { get; set; }
+    public string? Banco { get; set; }
+    public int CBU { get; set; }
+    // public double Saldo { get; set; }
+        private double Saldo; 
+
+    public string? Alias { get; set; }
+    public void Depositar(double monto) { 
+        Saldo+=monto;
+    }
+    public override void Retirar(double monto)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CtaCtr : Cuenta
+{
+    public int CBU { get; set; }
+    public double Descubierto { get; set; }
+    public void Retirar (){
+        
+    }
+        // public override void Retirar(double monto)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+public class CajaAhorro : Cuenta
+{
+    public bool Dolares { get; set; }
+    public double Descubierto { get; set; }
+    public override void Retirar(double monto)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+public class CuentaVirtual: Cuenta {
+    public string CVU { get; set; }
+    public override void Retirar(double monto)
+    {
+        throw new NotImplementedException();
+    }
+    public void ConsultarSaldo(double Depositar){
+
+}
+
+}
+
