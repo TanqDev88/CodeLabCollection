10e82d75695c7ca3ba0596769b2c92db06380123
tanqueta88 <oliverprim783@gmail.com>
2024-05-26 07:19:30 -0300

    Modifications in main program

--- POO/PC5POO/Clase5Solucion/Programa/Program.cs
+++ POO/PC5POO/Clase5Solucion/Programa/Program.cs
@@ -1,16 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Modelos;
 
-var piloto=new Piloto();
-piloto.Apellido="Papagudia";
-piloto.Nombre="Saracho";
-piloto.DNI=11111;
+var piloto = new Piloto();
+piloto.Apellido = "Papagudia";
+piloto.Nombre = "Saracho";
+piloto.DNI = 11111;
 
-var hunkers=new Avion();
+var hunkers = new Avion();
 hunkers.Abordar(piloto);
 
 piloto.SaludarPiloto();
-var pasajero1=new Pasajero(12345678,"Marina");
+var pasajero1 = new Pasajero(12345678, "Marina");
 // pasajero1.Nombre="Marina";
 // pasajero1.Apellido="Cacha";
 // pasajero1.DNI=124578;
@@ -18,3 +18,11 @@ var pasajero1=new Pasajero(12345678,"Marina");
 hunkers.Abordar(pasajero1);
 
 //var persona=new Persona();
+// piloto.cantidadDe Hacer capturas de este ejemplo de una propiedad privada
+
+piloto.HablarOficial();
+
+var azafaata1 = new Tcp();
+azafaata1.Nombre = "Clota";
+hunkers.Abordar(azafaata1);
+
