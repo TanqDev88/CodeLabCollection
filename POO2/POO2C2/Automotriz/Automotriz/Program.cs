a3a80125834f0ead9b2fb4246c129e8000a4276a
tanqueta88 <oliverprim783@gmail.com>
2024-06-02 18:12:03 -0300

    Automotriz file is added

---
+++ POO2/POO2C2/Automotriz/Automotriz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automotriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fordKa = new Automovil();
+            fordKa.Acelerar(410);
+
+            Automovil ds4 = new Automovil();
+            ds4.Acelerar(320);
+
+        }
+
+        //crear clases
+
+        class Automovil
+        {
+            string marca;
+            string modelo;
+            string color;
+            int año;
+            int velocidad_max;
+            int velocidad;
+
+            public void Acelerar(int kms)
+            {
+                this.velocidad = this.velocidad + kms;   //para acceder a los atributos propios de la clase en C# se utiliza la palabra reservada THIS
+            }
+        }
+    }
+
+}
+
