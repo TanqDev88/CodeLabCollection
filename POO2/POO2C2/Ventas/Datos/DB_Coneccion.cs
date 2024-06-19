2b806c19fa04de641dc0a07eadc675a7b64b3e6f
tanqueta88 <oliverprim783@gmail.com>
2024-06-19 01:44:14 -0300

    Initial commit

---
+++ POO2/POO2C2/Ventas/Datos/DB_Coneccion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; //Agregar esta referencia
+using System.Data.SqlClient; //Agregar esta otra referencia
+
+namespace Datos
+{
+    public class DB_Coneccion
+    {
+    }
+}
