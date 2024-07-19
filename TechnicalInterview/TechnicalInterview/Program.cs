c6d2e3c391e3314cb70640fcf2d394dd33e495a2
tanqueta88 <oliverprim783@gmail.com>
2024-07-18 23:16:59 -0300

    Test commit

--- TechnicalInterview/TechnicalInterview/Program.cs
+++ TechnicalInterview/TechnicalInterview/Program.cs
@@ -9,16 +9,21 @@ namespace TechnicalInterview
     internal class Program
     {
         static void Main(string[] args)
-        { //Invertir una cadena de texto
-            string text = "Pato";
-            string textResult = "";
+        //{ //Invertir una cadena de texto
+        //    string text = "Pato";
+        //    string textResult = "";
 
-            for (int i=text.Length-1; i>=0; i--)
-            {
-                textResult += text[i];
-            }
-            Console.WriteLine(textResult);
-            Console.ReadKey();
+        //    for (int i=text.Length-1; i>=0; i--)
+        //    {
+        //        textResult += text[i];
+        //    }
+        //    Console.WriteLine(textResult);
+        //    Console.ReadKey();
+        //}
+        {//2 Cuantas veces se repite un caracter
+            string text = "asdfasdflñkjasliea";
+            char charqacter = 'a';
+
         }
     }
 }
