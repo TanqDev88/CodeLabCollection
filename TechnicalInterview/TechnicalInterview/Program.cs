c941e7ede4e24c011f99a0d4a460701adec4f896
tanqueta88 <oliverprim783@gmail.com>
2024-07-18 23:08:05 -0300

    Added exercise invert a string.

---
+++ TechnicalInterview/TechnicalInterview/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalInterview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Invertir una cadena de texto
+            string text = "Pato";
+            string textResult = "";
+
+            for (int i=text.Length-1; i>=0; i--)
+            {
+                textResult += text[i];
+            }
+            Console.WriteLine(textResult);
+            Console.ReadKey();
+        }
+    }
+}
