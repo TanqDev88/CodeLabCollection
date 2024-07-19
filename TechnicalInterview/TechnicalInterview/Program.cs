3e84d1e56ac4e42eb05c01492414faf2b5cd4e4c
tanqueta88 <oliverprim783@gmail.com>
2024-07-18 23:29:24 -0300

    Revert "Test commit"
    This reverts commit c6d2e3c391e3314cb70640fcf2d394dd33e495a2.

--- TechnicalInterview/TechnicalInterview/Program.cs
+++ TechnicalInterview/TechnicalInterview/Program.cs
@@ -9,21 +9,16 @@ namespace TechnicalInterview
     internal class Program
     {
         static void Main(string[] args)
-        //{ //Invertir una cadena de texto
-        //    string text = "Pato";
-        //    string textResult = "";
+        { //Invertir una cadena de texto
+            string text = "Pato";
+            string textResult = "";
 
-        //    for (int i=text.Length-1; i>=0; i--)
-        //    {
-        //        textResult += text[i];
-        //    }
-        //    Console.WriteLine(textResult);
-        //    Console.ReadKey();
-        //}
-        {//2 Cuantas veces se repite un caracter
-            string text = "asdfasdflñkjasliea";
-            char charqacter = 'a';
-
+            for (int i=text.Length-1; i>=0; i--)
+            {
+                textResult += text[i];
+            }
+            Console.WriteLine(textResult);
+            Console.ReadKey();
         }
     }
 }
