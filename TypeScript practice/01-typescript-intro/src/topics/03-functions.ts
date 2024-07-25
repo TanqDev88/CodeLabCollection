46e4a1d1c70409601cda682d95c6ab15b62e0724
tanqueta88 <oliverprim783@gmail.com>
2024-07-25 04:44:43 -0300

    Modify 03-function file

--- TypeScript practice/01-typescript-intro/src/topics/03-functions.ts
+++ TypeScript practice/01-typescript-intro/src/topics/03-functions.ts
@@ -11,17 +11,18 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2){
     return firstNumber * base;
 }
 
-// const result: number = addNumbers(1,2)
-// const result2: string = addNumbersArrow(1,2)
-// const multiplyResult: number = multiply(5);
+const result: number = addNumbers(1,2)
+const result2: string = addNumbersArrow(1,2)
+const multiplyResult: number = multiply(5);
 
-// console.log({ result, result2, multiplyResult})
+console.log({ result, result2, multiplyResult})
 
 interface Character {
     name: string;
     hp: number;
     showHp: ()=> void;
 }
+
 
 const healCharacter = (character: Character, amount: number) =>{
     character.hp += amount;
