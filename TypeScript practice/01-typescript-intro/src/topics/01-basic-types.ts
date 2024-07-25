99d6cf206cf4e73a2293812a9b33064e4acf3799
tanqueta88 <oliverprim783@gmail.com>
2024-07-24 23:55:12 -0300

    Add basic-types and object-interface files

---
+++ TypeScript practice/01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+
+
+// const name = 'striker';
+const name: number = 1263;
+let htPoints: number | string = 98;
+
+htPoints = 'FULL';
+const isAlive: boolean = true;
+
+// name = '123';
+
+console.log({
+    name, htPoints, isAlive
+})
+
+export {}; 
