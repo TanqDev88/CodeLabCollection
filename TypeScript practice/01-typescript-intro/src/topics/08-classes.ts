35c34ea53cab2598f1a4d359f4b1daa7d098fbe3
TanqDev88 <oliverprim783@gmail.com>
2024-07-30 04:26:25 -0300

    Modifi 08-classes file

--- TypeScript practice/01-typescript-intro/src/topics/08-classes.ts
+++ TypeScript practice/01-typescript-intro/src/topics/08-classes.ts
@@ -2,15 +2,25 @@
 
 
 export class Person{
-    public name: string;
-    public adress: string;
+    // public name: string;
+    // public adress: string;
 
-    constructor(){
-        this.name = 'Fernando';
-        this.adress= 'Berlin'
+    constructor(
+        public name: string,
+        private address: string = 'No Address'
+    ){}
+}
+
+export class Hero extends Person{
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ){
+        super( realName, 'New York' );
     }
 }
 
-const ironman = new Person();
+const ironman = new Hero('Ironman', 45, 'Roberto Mosku');
 
-console.log(ironman.adress)
+console.log(ironman)
